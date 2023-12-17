aa9c85db972734a42b4554f1760e130bb6eb00f6
dromari <7709722@gmail.com>
2023-12-17 16:37:25 +0300

    feat remove and start binary

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -7,15 +7,22 @@ const { NotImplementedError } = require('../extensions/index.js');
 * using Node from extensions
 */
 class BinarySearchTree {
-
-  root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.root = null;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  root() {
+    return this.root
+  }
+
+  add(data) {
+    let newNode = new Node(data);
+    if (this.root == null){
+      this.root = newNode;
+    }
+    else {
+      
+    }
   }
 
   has(/* data */) {
