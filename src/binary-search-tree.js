413a3ba8b1b7119b07b155fdfe6cb86c4cf49982
dromari <7709722@gmail.com>
2023-12-25 00:20:27 +0300

    feat: binary tree

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
@@ -8,46 +8,146 @@ const { NotImplementedError } = require('../extensions/index.js');
 */
 class BinarySearchTree {
   constructor() {
-    this.root = null;
+    this.rootElement = null;
   }
 
   root() {
-    return this.root
+    return this.rootElement;
   }
 
   add(data) {
-    let newNode = new Node(data);
-    if (this.root == null){
-      this.root = newNode;
+    const newNode = new Node(data);
+    if (this.rootElement == null) {
+      this.rootElement = newNode;
     }
     else {
-      
+      this.createNode(this.rootElement, newNode);
     }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  createNode(current, newNode) {
+    if (current.data > newNode.data) {
+      if (current.left != null) {
+        this.createNode(current.left, newNode);
+      }
+      else {
+        current.left = newNode;
+      }
+    }
+    else {
+      if (current.right != null) {
+        this.createNode(current.right, newNode)
+      } else {
+        current.right = newNode
+      }
+    }
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    if (this.rootElement == null) {
+      return false;
+    }
+    let current = this.rootElement;
+    while (current != null) {
+      if (current.data == data) {
+        return true;
+      }
+      else if (current.data > data) {
+        current = current.left;
+      }
+      else {
+        current = current.right;
+      }
+    }
+    return false;
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data) {
+    if (this.rootElement == null) {
+      return null;
+    }
+    let current = this.rootElement;
+    while (current != null) {
+      if (current.data == data) {
+        return current;
+      }
+      else if (current.data < data) {
+        current = current.right;
+      }
+      else {
+        current = current.left;
+      }
+    }
+    return null;
+  }
+
+  remove(data) {
+    this.rootElement = this.deleteNode(this.rootElement, data);
+  }
+
+  deleteNode(node, data) {
+    if (!node) {
+      return null;
+    }
+
+    if (data < node.data) {
+      node.left = this.deleteNode(node.left, data);
+      return node;
+    } else if (data > node.data) {
+      node.right = this.deleteNode(node.right, data)
+      return node;
+    } else {
+      if (!node.left && !node.right) {
+        return null;
+      }
+      if (!node.left) {
+        node = node.right;
+        return node;
+      }
+
+      if (!node.right) {
+        node = node.left;
+        return node;
+      }
+
+      let minRight = node.right;
+      while (minRight.left) {
+        minRight = minRight.left;
+      }
+      node.data = minRight.data;
+      node.right = this.deleteNode(node.right, minRight.data)
+      return node;
+    }
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (this.rootElement == null) {
+      return null;
+    }
+    let current = this.rootElement;
+    while (current != null) {
+      if (current.left != null) {
+        current = current.left;
+      }
+      else {
+        return current.data;
+      }
+    }
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (this.rootElement == null){
+      return null;
+    }
+    let current = this.rootElement;
+    while (current != null){
+      if (current.right != null){
+        current = current.right;
+      }
+      else {
+        return current.data;
+      }
+    }
   }
 }
 
